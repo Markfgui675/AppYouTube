6f6ff06eb6a170757c48acb139bbecc807dea715
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-02-01 10:29:37 -0300

    Estilização
    Botões de ação e navegação inferior

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,7 @@
+import 'package:appyoutube/telas/biblioteca.dart';
+import 'package:appyoutube/telas/emalta.dart';
+import 'package:appyoutube/telas/incio.dart';
+import 'package:appyoutube/telas/inscricoes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,13 +13,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _indice = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricoes(),
+      Biblioteca()
+    ];
+
     return Scaffold(
 
       appBar: AppBar(
 
-        title: Text('Youtube', style: TextStyle(color: Colors.black),),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+          opacity: 0.75,
+        ),
+        title: Image.asset('imagens/youtube.png', width: 110,),
         actions: <Widget>[
           IconButton(onPressed: (){print('ação: videocam');}, icon: Icon(Icons.videocam, color: Colors.black)),
           IconButton(onPressed: (){print('ação: search');}, icon: Icon(Icons.search, color: Colors.black)),
@@ -25,7 +44,24 @@ class _HomeState extends State<Home> {
 
       ),
 
-      body: Text(''),
+      body: telas[_indice],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indice,
+        onTap: (indice){
+          setState(() {
+            _indice = indice;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Início'),
+          BottomNavigationBarItem(icon: Icon(Icons.whatshot), label: 'Em alta'),
+          BottomNavigationBarItem(icon: Icon(Icons.subscriptions), label: 'Inscrições'),
+          BottomNavigationBarItem(icon: Icon(Icons.folder), label: 'Biblioteca'),
+        ],
+      ),
 
     );
   }
